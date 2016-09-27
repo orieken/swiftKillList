4eec1667dbec76f334ca07939427a3bd94d28511
Oscar Rieken <oscar.rieken@adp.com>
2016-09-26 23:42:04 -0400

    Initial Commit

---
+++ dndKillList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dndKillList
+//
+//  Created by Rieken, Oscar on 9/26/16.
+//  Copyright © 2016 Rieken, Oscar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
